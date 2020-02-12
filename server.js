8c6e00bcd58ad501ec0c72b369e1b4a19b809824
Rene Ismael <Ismael_br7@hotmail.com>
2020-02-12 00:44:10 -0600

    Mejorando elÃ server y el response.js

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const response = require('./network/response');
+
 const router = express.Router();
 
 var app = express();
@@ -12,22 +15,26 @@ router.get('/message', function(req, res) {
   res.header({
     'custom-header': 'Nuestro valor personalizado',
   });
-  res.send('Hola desde get');
+  response.success(req, res, 'Lista de mensajes');
 });
 
 router.post('/message', function(req, res) {
   console.log(req.query);
   console.log(req.body);
-  //res.send('Mensage' + req.body.text + ' añadido correctamente');
-  res.status(201).send({
-    error: '',
-    body: 'Creado correctamente',
-  });
+  if ((req.query.error = 'ok')) {
+    response.error(
+      req,
+      res,
+      'Error inesperado',
+      500,
+      'Es solo una simulacion de los errores'
+    );
+  } else {
+    response.success(req, res, 'Creado correactamente', 201);
+  }
 });
 
-// app.use('/', function(req, res) {
-//   res.send('Hola');
-// });
+app.use('/app', express.static('public'));
 
 app.listen(3000);
 console.log('La aplicacion esta escuchando en http://localhost:3000');
