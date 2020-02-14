0673bebcc47ecde80693b527bea008a61b8e5dd0
IsmaelJdz <ismael_br7@hotmail.com>
2020-02-14 17:20:23 -0600

    creando dbTelegram en mongo, create and getData

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,25 @@
-const list = [];
+const db = require("mongoose");
+const Model = require("./model");
+//conexion de base de datos
+//mongodb:user:admin@localhost:27017/telegram
+//const list = [];
+
+db.Promise = global.Promise;
+db.connect("mongodb://localhost:27017/telegram", {
+  useNewUrlParser: true
+});
+console.log("[db] Conectada con éxito");
 
 function addMessage(message) {
-  list.push(message);
+  //list.push(message);
+  const myMessage = new Model(message);
+  myMessage.save();
 }
 
-function getMessages() {
-  return list;
+async function getMessages() {
+  //return list;
+  const messages = await Model.find();
+  return messages;
 }
 
 module.exports = {
