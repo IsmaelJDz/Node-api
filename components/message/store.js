3ac828d1b037e9ff2cc016005421bdb507de72fa
Rene Ismael <Ismael_br7@hotmail.com>
2020-02-17 00:43:31 -0600

    Creando componente chat

--- components/message/store.js
+++ components/message/store.js
@@ -2,26 +2,30 @@ const db = require("mongoose");
 const Model = require("./model");
 //conexion de base de datos
 //mongodb:user:admin@localhost:27017/telegram
-//const list = [];
-
-db.Promise = global.Promise;
-db.connect("mongodb://localhost:27017/telegram", {
-  useNewUrlParser: true
-});
-console.log("[db] Conectada con éxito");
 
 function addMessage(message) {
   const myMessage = new Model(message);
   myMessage.save();
 }
 
-async function getMessages(filterUser) {
-  let filter = {};
-  if (filterUser !== null) {
-    filter = { user: filterUser };
-  }
-  const messages = await Model.find(filter);
-  return messages;
+function getMessages(filterUser) {
+  return new Promise((resolve, reject) => {
+    let filter = {};
+    if (filterUser !== null) {
+      filter = { user: filterUser };
+    }
+    Model.find(filter)
+      //metodo que busca dentro de cada elemento que saquemos de la collecion de mongo
+      //object id y tenemos que decirle que campo y "ejecutar"
+      .populate("user")
+      .exec((err, populated) => {
+        if (err) {
+          reject(error);
+          return false;
+        }
+        resolve(populated);
+      });
+  });
 }
 
 async function updateText(id, message) {
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./db");
 
 //const router = require('./components/message/network');
-const router = require('./network/routes');
+const router = require("./network/routes");
+
+db("mongodb://localhost:27017/telegram");
 
 var app = express();
 app.use(bodyParser.json());
@@ -12,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //enviamos el server de express
 router(app);
 
-app.use('/app', express.static('public'));
+app.use("/app", express.static("public"));
 
 app.listen(3000);
-console.log('La aplicacion esta escuchando en http://localhost:3000');
+console.log("La aplicacion esta escuchando en http://localhost:3000");
